4b7905b221a06a5e2e84e740f7338cce3f6d9bec
pcarpe12 <pierrot.carpe@etudiant.mines-nantes.fr>
2014-11-05 15:35:33 +0100

    commit mille

--- TestGitPierrot.java
+++ TestGitPierrot.java
@@ -2,7 +2,11 @@
 public class TestGitPierrot {
 	
 	public static void main(String[] args) {
+<<<<<<< HEAD:TestGitPierrot.java
 		System.out.println("Swaggy swag fluffy fluf");
+=======
+		System.out.println("Bonjour");
+>>>>>>> commit:testPierrot.java
 	}
 
 }
